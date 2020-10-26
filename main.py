61fbcdad63ab4ed0fa6471733ef614e980c33fe5
Gachon-AIRLab <hshwang@gachon.ac.kr>
2020-10-26 15:20:06 +0900

    committttt

--- main.py
+++ main.py
@@ -4,9 +4,12 @@ from torchvision.models.detection.mask_rcnn import MaskRCNNPredictor
 from GilAAADataset import *
 from engine import train_one_epoch, evaluate
 import utils
+import pickle
 import cv2
 import transforms as T
 from torchvision.transforms import functional as F
+
+from gil_eval import *
 
 def get_instance_segmentation_model(num_classes):
     # load an instance segmentation model pre-trained on COCO
@@ -33,10 +36,18 @@ def main(mode):
     dataset_test = GilAAADataset('AAAGilDatasetPos', get_transform(train=False))
 
     # split the dataset in train and test set
-    torch.manual_seed(1)
-    indices = torch.randperm(len(dataset)).tolist()
-    dataset = torch.utils.data.Subset(dataset, indices[:-50])
-    dataset_test = torch.utils.data.Subset(dataset_test, indices[-50:])
+    # torch.manual_seed(1)
+    # indices = torch.randperm(len(dataset)).tolist()
+    # dataset = torch.utils.data.Subset(dataset, indices[:-50])
+    # dataset_test = torch.utils.data.Subset(dataset_test, indices[-50:])
+
+    # leave subject out
+    indices1 = list(range(0, 234))
+    indices2 = list(range(234, 314))
+    np.random.shuffle(indices1)
+    np.random.shuffle(indices2)
+    dataset = torch.utils.data.Subset(dataset, indices1)
+    dataset_test = torch.utils.data.Subset(dataset_test, indices2)
 
     # define training and validation data loaders
     data_loader = torch.utils.data.DataLoader(
@@ -81,6 +92,8 @@ def main(mode):
             evaluate(model, data_loader_test, device=device)
 
         torch.save(model.state_dict(), './pretrained/pretrained_weight.pth')
+        torch.save(dataset_test, './pretrained/dataset_test.pth')
+
 
     if 'test' in mode:
         model.load_state_dict(torch.load('./pretrained/pretrained_weight.pth'))
@@ -89,7 +102,7 @@ def main(mode):
         # print(img.shape)
         check_dir = '../AAAGilDatasetPos/'
         subject = '05390853_20200821'
-        img_idx = 86
+        img_idx = 96
 
 
         img_name = 'AAAGilDatasetPos/raw/' + subject + '_%04d.png'%img_idx
@@ -117,14 +130,88 @@ def main(mode):
 
         img_mask_gt = np.array(mask_gt)
 
+        img_mask[img_mask > 50] = 255
+        img_mask_gt_gray = cv2.cvtColor(img_mask_gt, cv2.COLOR_BGR2GRAY)
+
+        # cv2.imshow('input', img_in)
+        # cv2.imshow('mask result', img_mask)
+        # cv2.imshow('mask gt', img_mask_gt)
+
+        # segment evaluation
+        overlap, jaccard, dice, fn, fp = eval_segmentation(img_mask, img_mask_gt_gray)
+        print('[segmentation evaluation] overlab:%.4f jaccard:%.4f dice:%.4f fn:%.4f fp:%.4f'%(overlap, jaccard, dice, fn, fp))
 
 
-        cv2.imshow('input', img_in)
-        cv2.imshow('mask result', img_mask)
-        cv2.imshow('mask gt', img_mask_gt)
+        img_result = np.concatenate([img_mask, img_mask_gt_gray], axis=1)
+
+        img_overlap = img_mask_gt.copy()
+        img_overlap[:, :, 0] = 0
+        img_overlap[:,:,1] = img_mask
+        cv2.imshow('result', img_result)
+        cv2.imshow('overlap', img_overlap)
         cv2.waitKey(0)
+
+    if 'eval' in mode:
+        model.load_state_dict(torch.load('./pretrained/pretrained_weight.pth'))
+        dataset_test = torch.load('./pretrained/dataset_test.pth')
+
+        num_test = len(dataset_test.indices)
+        mat_eval = np.zeros((num_test, 5), np.float32)
+
+
+        for i in range(num_test):
+            img_name = 'AAAGilDatasetPos/raw/' + dataset_test.dataset.imgs[dataset_test.indices[i]]
+            mask_name = 'AAAGilDatasetPos/mask/' + dataset_test.dataset.imgs[dataset_test.indices[i]]
+
+            img = Image.open(img_name).convert("RGB")
+            mask_gt = Image.open(mask_name).convert("RGB")
+            img_rgb = np.array(img)
+            img = F.to_tensor(img)
+
+            model.eval()
+            with torch.no_grad():
+                prediction = model([img.to(device)])
+
+            im = Image.fromarray(img.mul(255).permute(1, 2, 0).byte().numpy())
+            mask = Image.fromarray(prediction[0]['masks'][0, 0].mul(255).byte().cpu().numpy())
+
+
+            img_mask = np.array(mask)
+            img_mask_gt = np.array(mask_gt)
+            img_mask[img_mask > 50] = 255
+            img_mask_gt_gray = cv2.cvtColor(img_mask_gt, cv2.COLOR_BGR2GRAY)
+
+            overlap, jaccard, dice, fn, fp = eval_segmentation(img_mask, img_mask_gt_gray)
+            print('[segmentation evaluation] overlab:%.4f jaccard:%.4f dice:%.4f fn:%.4f fp:%.4f' % (
+            overlap, jaccard, dice, fn, fp))
+
+            mat_eval[i, :] = [overlap, jaccard, dice, fn, fp]
+
+
+            img_gray  = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
+
+            img_result = np.concatenate([img_gray, img_mask, img_mask_gt_gray], axis=1)
+
+
+            img_overlap = img_mask_gt.copy()
+            img_overlap[:, :, 0] = 0
+            img_overlap[:, :, 1] = img_mask
+
+
+
+            cv2.imwrite('result/' + dataset_test.dataset.imgs[dataset_test.indices[i]].replace('.png', '_raw.png'), img_gray)
+            cv2.imwrite('result/' + dataset_test.dataset.imgs[dataset_test.indices[i]].replace('.png', '_mask_predict.png'), img_mask)
+            cv2.imwrite('result/' + dataset_test.dataset.imgs[dataset_test.indices[i]].replace('.png', '_mask_gt.png'), img_mask_gt_gray)
+            cv2.imwrite('result/' + dataset_test.dataset.imgs[dataset_test.indices[i]].replace('.png', '_overlap.png'), img_overlap)
+            cv2.imwrite('result/' + dataset_test.dataset.imgs[dataset_test.indices[i]].replace('.png', '_all.png'), img_result)
+
+            cv2.imshow('result', img_result)
+            cv2.imshow('overlap', img_overlap)
+            cv2.waitKey(10)
+
+        print(mat_eval.mean(axis=0))
 
 
 
 if __name__ == '__main__':
-    main('test')
+    main('eval')
